85a811145eab812dca860e77542c9162b2834b4a
yukiN0927 <kaifortysix_090@yahoo.co.jp>
2023-09-18 23:39:46 +0900

    test用

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from "react";
 
 function App() {
   let test = 1;
-  return (
-    <>
-      <button
-        onClick={() => {
-          test = test + 1;
-        }}
-      />
-      {test}
-    </>
-  );
+  return <>aaaaaaaaaaa</>;
 }
 
 export default App;
