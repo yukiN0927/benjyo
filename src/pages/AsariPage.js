fd25913ba5504df3f71bf0cf45cf05ab00f25511
yukiN0927 <kaifortysix_090@yahoo.co.jp>
2023-09-25 22:55:47 +0900

    [add]asari page upgrade

--- src/pages/AsariPage.js
+++ src/pages/AsariPage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function AsariPage() {
-  return <>AsariPage</>;
+export default function AsariPage() {
+  return (
+    <div>
+      <Box>
+        <AppBar>
+          <Toolbar>
+            <IconButton color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Typography>Profile</Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
 }
-
-export default AsariPage;
