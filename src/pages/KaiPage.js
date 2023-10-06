8f1075d77f236ff7890351137bf4eb0fddef5253
yukiN0927 <kaifortysix_090@yahoo.co.jp>
2023-10-06 18:09:01 +0900

    [add]BE構築

--- src/pages/KaiPage.js
+++ src/pages/KaiPage.js
@@ -3,15 +3,43 @@ import React from "react";
 function KaiPage(props) {
   // props
   const { setPage } = props;
+  const [favoriteCharacters, setFavoriteCharacters] = React.useState([]);
+
+  const fetchPosts = () => {
+    console.log("fetching-start");
+
+    fetch("https://yukin.pythonanywhere.com/benjyo/")
+      .then((response) => response.json())
+      .then((data) => {
+        setFavoriteCharacters(data);
+        console.log(data);
+      });
+  };
 
   return (
-    <button
-      onClick={() => {
-        setPage("Top");
-      }}
-    >
-      home
-    </button>
+    <>
+      <button
+        onClick={() => {
+          setPage("Top");
+        }}
+      >
+        Top
+      </button>
+      <button
+        onClick={() => {
+          fetchPosts();
+        }}
+      >
+        お気に入りキャラ取得
+      </button>
+      {favoriteCharacters.length > 0 && (
+        <>
+          {favoriteCharacters[0].number}
+          {favoriteCharacters[0].name}
+          {favoriteCharacters[0].character}
+        </>
+      )}
+    </>
   );
 }
 
