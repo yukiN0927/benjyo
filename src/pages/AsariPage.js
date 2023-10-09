521830631334baaf9211604b4725359dc6f66507
asariman <0gh38c3y16y5w1t@ezweb.ne.jp>
2023-10-09 21:12:07 +0900

    aaa

--- src/pages/AsariPage.js
+++ src/pages/AsariPage.js
@@ -1,24 +1,39 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-
+import errogo from "../image/errogo.jpg";
 export default function AsariPage() {
   return (
-    <div>
-      <Box>
-        <AppBar>
-          <Toolbar>
-            <IconButton color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Typography>Profile</Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
+    <>
+      <div>
+        <Box>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton color="inherit">
+                <MenuIcon />
+              </IconButton>
+              <Typography>Profile</Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </div>
+      <font size="5">
+        <p>name:asari</p>
+        <p>age:19</p>
+      </font>
+      <img src={errogo}></img>
+      <p>
+        <Button variant="contained" disableElevation>
+          Top
+        </Button>
+      </p>
+    </>
   );
 }
